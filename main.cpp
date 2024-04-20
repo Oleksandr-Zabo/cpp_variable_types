52e7dc524dfd75d0db6bba438783e073d6efab12
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-20 16:57:41 +0300

    ex. 2 lab work had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <windows.h> 
 using namespace std;
 
 int main()
 {
-	cout << "Hello world";
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, 0x0E);
+
+	cout << "\"U lukomor'a dub zelenij,\n "
+		<<"Zlataya zep na dupe tom,\n "
+		<<"I dnem i nochju kot uchenij\n "
+		<<"vse hodit po cepi krugom\""
+		<<endl;
+	SetConsoleTextAttribute(hConsole, 0x0A);
+
 	system("pause");
 	return 0;
 }
