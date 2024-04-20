a175d315447dcf7c2cd069700a1bb6ea4b6dbf35
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-20 16:49:14 +0300

    first ex. lab work had done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
-
 #include <iostream>
+#include <windows.h> 
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	SetConsoleTextAttribute(hConsole, 0x07);
+	cout << "\t\t\tName\t   : ";
+
+	SetConsoleTextAttribute(hConsole, 0x0E);
+	cout << "\"The war and the peace\" \n";
+
+
+	SetConsoleTextAttribute(hConsole, 0x07);
+	cout << "\t\t\tAvtor\t   :   L.N. Tolstoj\n";
+
+	cout << "\t\t\tIzdatelstvo:  Piter\n";
+
+	cout << "\t\t\tPages\t   : ";
+	SetConsoleTextAttribute(hConsole, 0x0D);
+	cout << "500\n";
+
+	SetConsoleTextAttribute(hConsole, 0x0A);
+
+
+	
+
+	system("pause");
+	return 0;
 }
