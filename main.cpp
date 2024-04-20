4c3160f67691da0ac7ddc02adf049009607b1bc0
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-20 18:30:08 +0300

    ex.2 hw had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-	cout << "Hello world";
+	double S;
+	float L, pi=3.14;
+	cout <<"Enter the length of the circle:  ";
+	cin >> L;
+	S = (L * L) / (4 * pi);
+	cout << "Circle area: "<< setprecision(8) << S << endl;
+
 	system("pause");
 	return 0;
 }
