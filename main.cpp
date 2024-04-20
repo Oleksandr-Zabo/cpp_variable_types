27d326b2353bd121cb03fd2f6bc211c6367afa80
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-20 17:41:29 +0300

    ex.3 started

--- main.cpp
+++ main.cpp
@@ -3,7 +3,82 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello world";
+	/*for (size_t i = 0; i <= 255; i++)
+	{
+		cout << i << " - " << char(i) << endl;
+	}*/
+
+	int left_top_line = 201;
+	int top_line = 205;
+	int right_top_line = 187;
+	int vertical_line = 186;
+	int left_top_right = 204;
+	int right_top_left = 185;
+	int center_down = 203;
+	int center_top = 202;
+	int down_right = 188;
+	int down_left = 200;
+
+	
+
+	cout <<char(left_top_line);
+	for (int i = 0; i < 84; i++) {
+		cout << char(top_line);
+	}
+	cout << char(right_top_line)<<endl;
+
+	cout << char(vertical_line) 
+		<<"                                  "
+		<<"Vremena goda"
+		<<"                                      " << char(vertical_line) 
+		<< endl;
+
+	cout << char(left_top_right);
+	for (int i = 0; i < 21; i++) {
+		cout << char(top_line);
+	}
+	cout << char(center_down);
+
+	cout << char(left_top_right);
+	for (int i = 0; i < 21; i++) {
+		cout << char(top_line);
+	}
+	cout << char(center_down);
+
+	cout << char(left_top_right);
+	for (int i = 0; i < 21; i++) {
+		cout << char(top_line);
+	}
+	cout << char(center_down);
+	cout << char(left_top_right);
+	for (int i = 0; i < 21; i++) {
+		cout << char(top_line);
+	}
+
+	cout << char(right_top_left) << endl;
+
+	cout << char(vertical_line)
+		<< "       "
+		<< "Zima"
+		<< "          " << char(vertical_line) 
+
+		<< "        " <<"Vesna"
+		<<"        " << char(vertical_line)
+
+		<< "         " << "Leto" 
+		<< "        " << char(vertical_line)
+
+		<< "       " << "Osen" 
+		<< "       " << char(vertical_line)
+		<< endl;
+
+
+	cout << char(down_left);
+	for (int i = 0; i < 84; i++) {
+		cout << char(top_line);
+	}
+	cout << char(down_right)<<"\n" << endl;
+
 	system("pause");
 	return 0;
 }
