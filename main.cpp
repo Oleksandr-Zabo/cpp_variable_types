dfb0d3c24b3c58447cac26c0aebff0cc36e16ab9
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-20 18:40:33 +0300

    ex.3 hw had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-	cout << "Hello world";
+	double S;
+	float V, t, a;
+	cout << "Enter the speed (V): ";
+	cin >> V;
+	cout << "Enter the time (t): ";
+	cin >> t;
+	cout << "Enter acceleration (a): ";
+	cin >> a;
+	S = V * t + (a * t * t) / 2;
+	cout << "Displacement (S) = "
+		<< setprecision(5) <<S<<endl;  
 	system("pause");
 	return 0;
 }
